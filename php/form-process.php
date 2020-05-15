5991477cf3e2359f92d13f1d600542b2ae2b9d70
Creavic Design <Victor@MacBook-Pro-de-Victor.local>
2020-05-15 12:16:57 -0500

    php

--- php/form-process.php
+++ php/form-process.php
@@ -16,14 +16,6 @@ if (empty($_POST["email"])) {
     $email = $_POST["email"];
 }
 
-// MSG SUBJECT
-if (empty($_POST["msg_subject"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $msg_subject = $_POST["msg_subject"];
-}
-
-
 // MESSAGE
 if (empty($_POST["message"])) {
     $errorMSG .= "Message is required ";
@@ -32,21 +24,18 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "armanmia7@gmail.com";
-$Subject = "New Message Received";
+$EmailTo = "victorjativa@hotmail.com";
+$Subject = "Mensaje de la WEB";
 
 // prepare email body text
 $Body = "";
-$Body .= "Name: ";
+$Body .= "Nombre: ";
 $Body .= $name;
 $Body .= "\n";
-$Body .= "Email: ";
+$Body .= "Correo: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "Subject: ";
-$Body .= $msg_subject;
-$Body .= "\n";
-$Body .= "Message: ";
+$Body .= "Mensaje: ";
 $Body .= $message;
 $Body .= "\n";
 
@@ -55,10 +44,10 @@ $success = mail($EmailTo, $Subject, $Body, "From:".$email);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
-   echo "success";
+   echo "Enviado";
 }else{
     if($errorMSG == ""){
-        echo "Something went wrong :(";
+        echo "Ocurrio un problema";
     } else {
         echo $errorMSG;
     }
