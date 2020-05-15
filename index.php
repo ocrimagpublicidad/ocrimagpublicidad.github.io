f5fab33eebb67e9652efd613ea59176d7dff7d93
Creavic Design <Victor@MacBook-Pro-de-Victor.local>
2020-05-15 14:59:41 -0500

    correccion

--- index.php
+++ index.php
@@ -1099,63 +1099,6 @@
     </section>
     <!-- Contact Section End -->
 
-    <?php
-    $errorMSG = "";
-
-    // NAME
-    if (empty($_POST["name"])) {
-        $errorMSG = "Name is required ";
-    } else {
-        $name = $_POST["name"];
-    }
-
-    // EMAIL
-    if (empty($_POST["email"])) {
-        $errorMSG .= "Email is required ";
-    } else {
-        $email = $_POST["email"];
-    }
-
-    // MESSAGE
-    if (empty($_POST["message"])) {
-        $errorMSG .= "Message is required ";
-    } else {
-        $message = $_POST["message"];
-    }
-
-
-    $EmailTo = "victorjativa@hotmail.com";
-    $Subject = "Mensaje de la WEB";
-
-    // prepare email body text
-    $Body = "";
-    $Body .= "Nombre: ";
-    $Body .= $name;
-    $Body .= "\n";
-    $Body .= "Correo: ";
-    $Body .= $email;
-    $Body .= "\n";
-    $Body .= "Mensaje: ";
-    $Body .= $message;
-    $Body .= "\n";
-
-    // send email
-    $success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-    // redirect to success page
-    if ($success && $errorMSG == ""){
-      echo "Enviado";
-    }else{
-        if($errorMSG == ""){
-            echo "Ocurrio un problema";
-        } else {
-            echo $errorMSG;
-        }
-    }
-
-    ?>
-
-
     <!-- Footer Section Start -->
     <footer>          
       <div class="container">
